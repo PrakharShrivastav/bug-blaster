65b73f6758ccd7a26d1920ac067198b8a19a4c10
Prashant Shrivastava <psanimes@gmail.com>
2025-05-25 01:30:02 +0530

    Added the edit functionality

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import ticketReducer from "./reducers/ticketReducer";
 import TicketList from "./components/TicketList";
 
 function App() {
-  const initialState = { tickets: [] };
+  const initialState = { tickets: [], editingTicket: null };
   const [state, dispatch] = useReducer(ticketReducer, initialState);
   return (
     <div className="App">
       <div className="container">
         <h1>Bug Blaster</h1>
-        <TicketForm dispatch={dispatch} />
+        <TicketForm
+          dispatch={dispatch}
+          editingTicket={state.editingTicket}
+        ></TicketForm>
         {state.tickets.length > 0 && (
           <div className="results">
             <h2>All Tickets</h2>
